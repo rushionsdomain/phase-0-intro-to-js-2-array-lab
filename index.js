9264364de0e0e43de3baea79aa8088e391f5f6da
Rushion <rushionchegge@gmail.com>
2024-06-30 23:18:07 +0300

    edited js file, added code

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+function appendCat(name) {
+  // Create a new array by concatenating the original cats array with the new name
+  return [...cats, name];
+}
+
+function prependCat(name) {
+  // Create a new array by spreading the new name followed by the original cats array
+  return [name, ...cats];
+}
+
+function removeLastCat() {
+  // Create a new array by slicing the original cats array to exclude the last element
+  return cats.slice(0, cats.length - 1);
+}
+
+function removeFirstCat() {
+  // Create a new array by slicing the original cats array to exclude the first element
+  return cats.slice(1);
+}
+
+module.exports = {
+  cats,
+  destructivelyAppendCat,
+  destructivelyPrependCat,
+  destructivelyRemoveLastCat,
+  destructivelyRemoveFirstCat,
+  appendCat,
+  prependCat,
+  removeLastCat,
+  removeFirstCat,
+};
